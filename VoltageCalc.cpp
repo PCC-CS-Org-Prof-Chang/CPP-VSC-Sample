a3ce3e31c60f65914c4e956ddf40b8eafab18604
Ken Chang - CS Professor <154487739+adjprof2@users.noreply.github.com>
2024-09-04 20:05:07 -0700

    enhanced version

--- VoltageCalc.cpp
+++ VoltageCalc.cpp
@@ -10,5 +10,12 @@ int main() {
     double volts = millivolts / 1000.0;;
     std::cout << "The voltage in volts is: " << volts << std::endl;
 
+    double current;
+    std::cout << "Enter the current in milliamps: ";
+    std::cin >> current;
+
+    double amps = current / 1000.0;
+    cout << "The current in amps is: " << amps << endl; 
+
     return 0;
 }
